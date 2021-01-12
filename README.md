6ca54fe607fd144fe6189dee5fb09ba82d5e2b98
Rohan Robinson <rohan.c.robinson.21@dartmouth.edu>
2021-01-12 16:56:39 -0600

    first client commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# hack-a-thing-1-client
+
+# created for COSC 98.01, Winter Term 2021
+
+## by Rohan Robinson
---
+++ index.js
@@ -0,0 +1,7 @@
+const API_URL = 'http://localhost:3000/';
+
+fetch(API_URL)
+    .then(res => res.json())
+    .then(result => {
+        console.log(result);
+    });
